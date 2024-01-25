57816c81f13bde37834b4f7d4c100f634b841c00
Carlos A Carvalho <carlinhosarena@gmail.com>
2024-01-25 11:04:41 -0300

    Add project files.

---
+++ MultiTenant.Middleware.Api/Program.cs
@@ -0,0 +1,22 @@
+
+namespace MultiTenant.Middleware.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+            return builder;
+        }
+    }
+}
---
+++ MultiTenant.Middleware.Api/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace MultiTenant.Middleware.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
